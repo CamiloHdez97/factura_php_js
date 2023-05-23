6cb10886f2211d1953c672dc0b6a2240fceacc4e
Juan <juseumca@gmail.com>
2023-05-23 09:31:40 -0500

    Clase 1

---
+++ template/header.php
@@ -0,0 +1,21 @@
+<?php 
+    $url_base="http://localhost";
+?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="js/boostrap/bootstrap.min.js" defer></script>
+    <title>Factura</title>
+</head>
+<body>
+
+<nav class="nav justify-content-center  ">
+  <a class="nav-link active" href="#" aria-current="page">Active link</a>
+  <a class="nav-link" href="#">Link</a>
+  <a class="nav-link disabled" href="#">Disabled link</a>
+</nav>
