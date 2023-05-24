19b79088902b93f8e08bfd61236152eddd289a14
angelgabrielortegacorzoCAMPUS <angelgabrielorteg@gmail.com>
2023-05-24 18:21:44 -0500

    feat(nueva interfaz y reactividad con js): :rocket: mejoras
    se añadio funcion de eliminar y correccion a algunos errores en funcion de añadir filas
    
    mejoras eficientes y diseño

--- js/app.js
+++ js/app.js
@@ -1,37 +1,69 @@
-let num=2;
-document.querySelector("#addItem").addEventListener("click",(e)=>{
-    num=num++
-    let tbody = document.querySelector('#addItemtbody');
-    let tr = document.createElement('tr');
-    tr.innerHTML = /* html */`
-        <tr>
-            <th scope="row" id="${num}">${num}</th>   
-            <td>
-                <input required="" type="text" name="${num}[]" class="input">
-            </td>            
-            <td>                        
-                <input required="" type="text" name="${num}[]" class="input">
-            </td>
-            <td>                       
-                <input required="" type="text" name="${num}[]" class="input">
-            </td>
-            <td>                       
-                <input required="" type="text" name="${num}[]" class="input">
-            </td>
-            <td>                       
-                <a name="" id="" class="btn btn-success" href="#" role="button">+</a>
-                <a name="" id="" class="btn btn-danger" href="#" role="button">-</a>
-            </td>
-        </tr>
-    `;
+//funcion añadir filas
+let num = 1;
 
-    tbody.appendChild(tr);
+function agregarFila() {
+  let tbody = document.querySelector('#addItemtbody');
+  num++;
 
-    console.log('entra');
-    e.preventDefault();
-    e.stopPropagation();
+  let tr = document.createElement('tr');
+  tr.innerHTML = /* html */`
+    <tr>
+      <th scope="row" id="${num}">${num}</th>   
+      <td>
+        <input required="" type="text" name="${num}[]" class="input">
+      </td>            
+      <td>                        
+        <input required="" type="text" name="${num}[]" class="input">
+      </td>
+      <td>                       
+        <input required="" type="text" name="${num}[]" class="input">
+      </td>
+      <td>                       
+        <input required="" type="text" name="${num}[]" class="input">
+      </td>
+      <td>                       
+        <a name="" id="" class="btn btn-success" href="#" role="button">+</a>
+        <a name="" id="" class="btn btn-danger" href="#" role="button">-</a>
+      </td>
+    </tr>
+  `;
 
-})
+  tbody.appendChild(tr);
+
+  console.log('entra');
+
+  return num;
+}
+
+document.querySelector("#addItemtbody").addEventListener("click", (e) => {
+  if (e.target.classList.contains('btn-success')) {
+    agregarFila();
+  } else if (e.target.classList.contains('btn-danger')) {
+    const row = e.target.parentNode.parentNode;
+    const position = parseInt(row.querySelector('th').getAttribute('id'));
+    row.remove();
+    num--;
+    return position;
+  }
+
+  e.preventDefault();
+  e.stopPropagation();
+});
+
+
+
+
+
+//funcion para eliminar filas
+
+document.querySelector('#addItemtbody').addEventListener('click', (e) => {
+    if (e.target.classList.contains('btn-danger')) {
+      const row = e.target.parentNode.parentNode;
+      row.remove();
+    }
+  });
+  
+
 
 //var myModal = document.querySelector('.mymodal')
 //var myInput = document.querySelector('.boton')
