00588145801a9747faf4dcd7160797f24654e0d4
angelgabrielortegacorzoCAMPUS <angelgabrielorteg@gmail.com>
2023-05-23 19:55:41 -0500

    feat(diseño base): :art: base inicial
    en la base se añadio las primeras instancias para hacer la interfaz inicial
    
    detalles

--- template/header.php
+++ template/header.php
@@ -14,8 +14,8 @@
 </head>
 <body>
 
-<nav class="nav justify-content-center  ">
-  <a class="nav-link active" href="#" aria-current="page">Active link</a>
-  <a class="nav-link" href="#">Link</a>
-  <a class="nav-link disabled" href="#">Disabled link</a>
+<nav class="nav justify-content-center">
+  <a class="nav-link active" href="#" aria-current="page">Principal</a>
+  <a class="nav-link" href="#">Compras</a>
+  <a class="nav-link disabled" href="#">Resumenes de compra</a>
 </nav>
